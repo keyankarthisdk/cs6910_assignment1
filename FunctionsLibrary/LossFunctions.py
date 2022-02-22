7c6ff5b1665350822c1d08af7782b8926f92684b
keyankarthisdk <keyankarthisdk@gmail.com>
2022-02-22 22:31:42 +0530

    Added Loss functions and derivatives of loss functions (Mean Squared Error and cross entropy)

--- FunctionsLibrary/LossFunctions.py
+++ FunctionsLibrary/LossFunctions.py
@@ -8,9 +8,34 @@ import numpy as np
 # Main Functions
 # Loss Functions
 # @ Karthikeyan S CS21M028
-# TODO: Mean Squared Error, Cross Entropy
-
 
 # Derivatives Functions
-# @ Karthikeyan S CS21M028
-# TODO: Mean Squared Error, Cross Entropy
+def LossFunc_MeanSquaredError(y, t, **params):
+    '''
+    Mean Squared Error
+    '''
+    loss = 0.5 * np.sum((y-t)**2, axis=-1)
+    loss = np.mean(loss)
+    return loss
+
+def LossFunc_CrossEntropy(y, t, **params):
+    '''
+    Cross Entropy
+    '''
+    y = np.clip(y, 1e-15, 1.0)
+    loss = -np.sum(t * np.log(y + (t == 0)), axis=-1)
+    loss = np.mean(loss)
+    return loss
+
+# Derivatives Functions
+def LossFuncDeriv_MeanSquaredError_WithSoftmax(y, t, **params):
+    '''
+    Mean Squared Error Derivative
+    '''
+    return (y - t) * y * (1 - y)
+
+def LossFuncDeriv_CrossEntropy_WithSoftmax(y, t, **params):
+    '''
+    Cross Entropy Derivative
+    '''
+    return y - t
