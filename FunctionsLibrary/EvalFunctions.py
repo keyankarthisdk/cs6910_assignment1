aa5ecf6541a0ff585a39c657d4247407d6effddd
Kausik N <nkausik1999@gmail.com>
2022-02-22 16:47:21 +0530

    added ActivationFuncs and Derivs: sigmoid, tanh, relu, softmax, added Accuracy Eval Func

--- FunctionsLibrary/EvalFunctions.py
+++ FunctionsLibrary/EvalFunctions.py
@@ -8,4 +8,8 @@ import numpy as np
 # Main Functions
 # Eval Functions
 # @ N Kausik CS21M037
-# TODO: Accuracy
+def EvalFunc_Accuracy(y, t, **params):
+    '''
+    Calculates the accuracy of the model
+    '''
+    return np.sum(t == y)# / float(t.shape[0])
