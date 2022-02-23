b9e95d8c8e3de03c15fbf9ae10e2038b4452f495
keyankarthisdk <keyankarthisdk@gmail.com>
2022-02-23 09:18:17 +0530

    Added code to initialize parameters for the neural network.

--- FNN.py
+++ FNN.py
@@ -15,7 +15,23 @@ import wandb
 
 # Main Functions
 # @ Karthikeyan S CS21M028
-# TODO: Model Parameters Initialize
+# Init Functions
+def Params_Init(layer_sizes, funcs):
+    Ws = []
+    bs = []
+    Ws, bs = funcs["init_fn"]["func"](layer_sizes, **funcs["init_fn"]["params"])
+
+    parameters = {
+        "n_layers": len(layer_sizes),
+        "layer_sizes": layer_sizes,
+        "Ws": Ws,
+        "bs" : bs,
+        "act_fns": funcs["act_fns"],
+        "loss_fn": funcs["loss_fn"]
+    }
+    for i in range(len(Ws)):
+        print(i, "W:", (Ws[i].min(), Ws[i].max()), "b:", (bs[i].min(), bs[i].max()))
+    return parameters
 
 
 # @ N Kausik CS21M037
